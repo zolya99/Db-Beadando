cd6fedea457688f8337af289931215abd8a99e71
Nagy Zoltan <zolya1999@gmail.com>
2021-01-18 19:35:00 +0100

    Added Tables.sql

---
+++ sql/Tables.sql
@@ -0,0 +1,41 @@
+CREATE TABLE Csapat(
+id int PRIMARY KEY,
+nev varchar2(50));
+
+CREATE TABLE Poszt(
+id int PRIMARY KEY,
+megnevezes varchar2(50));
+
+CREATE TABLE Focista(
+id int PRIMARY KEY,
+szuletesi_ev int,
+vezeteknev varchar2(50),
+keresztnev varchar2(50));
+
+CREATE TABLE Tagja(
+Focista int references Focista(id),
+Csapat int references Csapat(id),
+Mezszam int,
+primary key (Focista, Csapat, Mezszam));
+
+CREATE TABLE Jatszik(
+Focista int references Focista(id),
+Poszt int references Poszt(id),
+primary key (Focista, Poszt));
+commit;
+
+
+INSERT INTO Csapat VALUES ('1', 'Zamora FC'); 
+INSERT INTO Csapat VALUES ('2', 'Ferlach'); 
+INSERT INTO Csapat VALUES ('3', 'Cornella'); 
+INSERT INTO Csapat VALUES ('4', 'Atlético Madrid'); 
+INSERT INTO Csapat VALUES ('5', 'Lugo'); 
+
+INSERT INTO Poszt VALUES ('1', 'Csatár');
+INSERT INTO Poszt VALUES ('2', 'Védõ');
+INSERT INTO Poszt VALUES ('5', 'Kapus');
+INSERT INTO Poszt VALUES ('6','Középpályás');
+
+Insert Into Focista Values ('1', '1995', 'Végh', 'Béla');
+
+Insert Into Tagja Values ('4', '2', '2');
